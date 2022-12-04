c53f2a2700e1e91a185ba8aeff40c34c49f25e64
matheuscosme <mcosme.tavares@gmail.com>
2022-12-04 03:03:39 -0300

    muitas alterações

--- src/index.js
+++ src/index.js
@@ -6,99 +6,161 @@ server.use(express.json());
 server.use(cors());
 const mongoClient = require('mongodb').MongoClient;
 
-const APP_PORT = 8080;
+const APP_PORT = 3001;
 const APP_HOST = 'localhost';
 const MONGO_HOST = 'mongodb+srv://cosme_tavares:123456M@cluster0.fr015va.mongodb.net/?retryWrites=true&w=majority';
 const MONGO_DB = 'spotify';
-const MONGO_COLLECTION = 'playlists';
+const COLLECTION_PLAYLISTS = 'playlists';
+const COLLECTION_USERS = 'usuarios';
+const COLLECTION_PLAY_USERS = 'playlistsDeUsuarios';
+const COLLECTION_MUSICAS = 'musicas';
 
 
 server.listen(APP_PORT, APP_HOST);
 
-server.get('/playlists', (req, res) => {   
+server.get('/users', (req, res) => {   
   mongoClient.connect(MONGO_HOST, (err, client) => {
     if (err) throw err
     const database = client.db(MONGO_DB);
-    database.collection(MONGO_COLLECTION).find().toArray((err, result) => {
+    database.collection(COLLECTION_USERS).find().toArray((err, result) => {
       if (err) throw err
       res.send(result);
     });
   });
 });
 
+
+server.get('/users/:email', (req,res)=>{
+  const {email} = req.params
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_USERS).findOne({ email: email }, (err, result) => {
+      if (err) throw err
+      res.send(result);
+    });
+  });
+});
+
+
+server.post('/users', (req, res) => {
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_USERS).insertOne(req.body, (err) => {
+      if (err) throw err
+      res.status(201);
+      res.send();
+    });
+  });
+});
+
+
+server.put('/users/:id', (req, res) => {
+  const {id} = req.params
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_USERS).updateOne({ _id: ObjectId(id) }, { $set: {"usuario" : req.body.usuario , 
+                                                                                    "email" : req.body.email ,
+                                                                                    "senha" : req.body.senha} }, (err) => {
+      if (err) throw err
+      res.send();
+    });
+  });
+});
+
+
+server.get('/playlists', (req, res) => {   
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_PLAYLISTS).find().toArray((err, result) => {
+      if (err) throw err
+      res.send(result);
+    });
+  });
+});
+
+server.get('/playlistsDeUsuarios', (req, res) => {   
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_PLAY_USERS).find().toArray((err, result) => {
+      if (err) throw err
+      res.send(result);
+    });
+  });
+});
+
+server.post('/playlistsDeUsuarios', (req, res) => {
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_PLAY_USERS).insertOne(req.body, (err) => {
+      if (err) throw err
+      res.status(201);
+      res.send();
+    });
+  });
+});
+
+
 server.get('/playlist/:id', (req,res)=>{
     const {id} = req.params
     mongoClient.connect(MONGO_HOST, (err, client) => {
       if (err) throw err
       const database = client.db(MONGO_DB);
-      database.collection(MONGO_COLLECTION).findOne({ _id: ObjectId(id) }, (err, result) => {
+      database.collection(COLLECTION_PLAYLISTS).findOne({ _id: ObjectId(id) }, (err, result) => {
         if (err) throw err
         res.send(result);
       });
     });
 });
 
-// server.get('/playlistsUser', (req, res) => {   
-//     return res.json(playlistsDeUsuarios);
-// });
 
-// server.get('/playlistsUser/:id', (req,res)=>{
-//     const {id} = req.params
-//     res.json(playlistsDeUsuarios[id-1])
-// });
-
-// server.post('/usuarios', (req, res) => {
-//     const user = req.body
-//     Usuarios.push(user)
-//     return res.status(200).json(Usuarios)
-// });
-
-// server.get('/usuarios', (req,res)=>{
-//     const {email} = req.query
-//     for (let i = 0; i<Usuarios.length ; i++){
-//       if(Usuarios[i].email == email){
-//         var usuario = Usuarios[i]
-//       }
-//     }
-//     return res.json(usuario)
-// })
-
-// server.put('/usuarios/:id', (req,res)=>{
-//     var {id} = req.params;
-//     id = parseInt(id)
-//     Usuarios[id-1] = req.body;
-//     return res.json(Usuarios);
-// })
+server.get('/todasAsMusicas', (req, res) => {   
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_MUSICAS).find().toArray((err, result) => {
+      if (err) throw err
+      res.send(result);
+    });
+  });
+});
 
 
-// server.post('/criarPlay', (req, res) => {
-//     const playlist = req.body
-//     playlistsDeUsuarios.push(playlist)
-//     return res.status(200).json(playlistsDeUsuarios)
-// });
-
-// server.get('/musicas', (req,res)=>{
-//   let array = []
-//   const {busca} = req.query
-//   for(let i=0;i<musicas.length;i++){
-//       if(musicas[i].nome.toLowerCase().indexOf(busca.toLowerCase())>-1){
-//           array.push(musicas[i])
-//       }
-//       else if(musicas[i].artista.toLowerCase().indexOf(busca.toLowerCase())>-1){
-//         array.push(musicas[i])
-//     }
-//   }
-//     return res.json(array)
-// })
-
-// server.get('/TodasAsMusicas', (req,res)=>{
-//     return res.json(musicas)
-// })
+server.get('/todasAsMusicas/:busca', (req,res)=>{
+  let array = []
+  const {busca} = req.params
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_MUSICAS).find().toArray((err, result) => {
+      for(let i=0;i<result.length;i++){
+              if(result[i].nome.toLowerCase().indexOf(busca.toLowerCase())>-1){
+                  array.push(result[i])
+              }
+              else if(result[i].artista.toLowerCase().indexOf(busca.toLowerCase())>-1){
+                array.push(result[i])
+            }
+          }
+      if (err) throw err
+      res.send(array);
+    });
+  });
+});
 
 
-// server.put('/playlistsUser/:id', (req,res)=>{
-//   var {id} = req.params;
-//   id = parseInt(id)
-//   playlistsDeUsuarios[id-1] = req.body;
-//   return res.json(playlistsDeUsuarios);
-// })
+server.put('/playlistsDeUsuarios/:id', (req, res) => {
+  const {id} = req.params
+  mongoClient.connect(MONGO_HOST, (err, client) => {
+    if (err) throw err
+    const database = client.db(MONGO_DB);
+    database.collection(COLLECTION_USERS).updateOne({ _id: ObjectId(id) }, { $set: {"musicas" : req.body.musicas} }, (err) => {
+      if (err) throw err
+      res.send();
+    });
+  });
+});
